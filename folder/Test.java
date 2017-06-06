4810e45a50f126a552997c63cc883c3bc7acc31f
Discombob <discombob@users.noreply.github.com>
2017-06-05 21:53:26 -0400

    Add some comment

--- folder/Test.java
+++ folder/Test.java
@@ -1,6 +1,10 @@
 /* Just writing some test stuff */
 public class Test {
 	
+	/**
+	 * This method simply outputs the argument to the console.
+	 * @param args the argument input
+	 * */
 	public static void main(String[] args) {
 		Arrays.asList(args).forEach(System.out::println);
 	}
