06ddaaccb0aeae93754868f6abd88d99ad86ce0b
Discombob <discombob@users.noreply.github.com>
2017-06-24 22:51:41 -0400

    Add age check to constructor

--- folder/sub-folder/Person.java
+++ folder/sub-folder/Person.java
@@ -1,13 +1,21 @@
 import java.util.Objects;
 
+/**
+ * Represents a basic person that can do absolutely nothing incredible.
+ */
 public class Person {
 	
 	private String name;
 	private int age;
 	
+	/**
+	 * @param name the name of the person
+	 * @param age the age of the person
+	 * @throws NullPointerException if the name is null
+	 */
 	public Person(String name, int age) {
 		this.name = Objects.requireNonNull(name);
-		this.age = age;
+		this.age = (age < 1) ? 1 : age;
 	}
 	
 	public String getName() { return name; }
